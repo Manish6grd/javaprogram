5ee13866624442953d162e57db609ddee4b1789a
Manish6grd <70393318+Manish6grd@users.noreply.github.com>
2025-10-30 22:56:16 +0530

    Implement leap year check in GeeksforGeeks class
    Added leap year checking functionality and comments.

--- SumOfDigits.java
+++ SumOfDigits.java
@@ -1,30 +1,53 @@
-public class SumOfDigits{
-public static void main(String[] args){
-int n = 57;
-int d;
-int sum=0;
-while(n!=0){
-	d=n%10;
-	sum=sum+d;//new comment
-	n=n/10;//new line
-	//in trial
-	
+// Java program to find a leap year
+// Importing Classes/Files
+import java.io.*;
+
+// Class for leap-year dealing
+public class GeeksforGeeks {
+    // Method to check leap year
+    public static void isLeapYear(int year)
+    {
+        // flag to take a non-leap year by default
+        boolean is_leap_year = false;
+
+        // If year is divisible by 4
+        if (year % 4 == 0) {
+            is_leap_year = true;
+
+            // To identify whether it is a
+            // century year or not
+            if (year % 100 == 0) {
+                // Checking if year is divisible by 400
+                // therefore century leap year
+                if (year % 400 == 0)
+                    is_leap_year = true;
+                else
+                    is_leap_year = false;
+            }
+        }
+
+        // We land here when corresponding if fails
+        // If year is not divisible by 4
+        else
+
+            // Flag dealing-  Non leap-year
+            is_leap_year = false;
+
+        if (!is_leap_year)
+            System.out.println(year + " : Non Leap-year");
+        else
+            System.out.println(year + " : Leap-year");
+    }
+
+    // Driver Code
+    public static void main(String[] args)
+    {
+        // Calling our function by
+        // passing century year not divisible by 400
+        isLeapYear(2000);
+
+        // Calling our function by
+        // passing Non-century year
+        isLeapYear(2002);
+    }
 }
-System.out.println(sum);
-	SOP;
-}
-
-
-}
-added by b
-20
-added by a
-a1
-a1 line 21
-a1 line 22
-	23
-	24
-
-28
-27
-
